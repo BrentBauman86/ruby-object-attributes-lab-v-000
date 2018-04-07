3d865368c478e7fae8317efdf105141277534cdd
Brent Bauman <brentbauman86@gmail.com>
2018-04-07 16:34:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
 
   def name=(dogs_name)
-    @this_dogs_name = dogs_name
+    @name = dogs_name
   end
 
   def name
-    @this_dogs_name
+    @name
   end
 end
 
 fido = Dog.new
 fido.name = "Fido"
-puts fido.name
+fido.name
