d8c7299356c56c9d6e7b994eda7307d5eabb7a4d
Brent Bauman <brentbauman86@gmail.com>
2018-04-07 15:19:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,8 @@
 class Dog
-  def initialize(dog_name)
-    @dog_name = dog_name
-end
+  def initialize(name)
+    @name = name
+  end
 
-def dog_name
-  @dog_name
-
-def name=(new_dog_name)
-    @name = new_dog_name
-end
-
-end
-end
-
-fido = Dog.new("fido")
-fido.name
+  def name
+    @name
+  end
